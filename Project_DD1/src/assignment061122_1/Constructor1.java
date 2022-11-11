38adb2bf77ac739632e49d1aa2466922b7f43a33
DebTintin <117949147+DebTintin@users.noreply.github.com>
2022-11-12 01:07:11 +0200

    My Second code push

---
+++ Project_DD1/src/assignment061122_1/Constructor1.java
@@ -0,0 +1,39 @@
+package assignment061122_1;
+
+public class Constructor1 
+{
+	
+  public Constructor1()
+  {
+	this (10, 20, 30, 40);
+	System.out.println("Default Constructor");
+  }
+  public Constructor1(int a)
+  {
+	this ();
+	System.out.println("One Parameterized Constructor");
+  }
+  
+  public Constructor1(int a, int b)
+  {
+	this (22, 33, 44);
+	System.out.println("Two Parameterized Constructor");
+  }
+  
+  public Constructor1(int a, int b, int c)
+  {
+	this (10);
+	System.out.println("Three Parameterized Constructor");
+  }
+  
+  public Constructor1(int a, int b, int c, int d)
+  {
+	System.out.println("Four Parameterized Constructor");
+  }
+  
+public static void main(String[] args)
+{
+  Constructor1 obj1 = new Constructor1 (33, 44);
+}
+  
+}
